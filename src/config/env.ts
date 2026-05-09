dc974010ce3ed23a42b295aab6234f7b6592a9d4
I'm Folajimi <62119915+Folexy13@users.noreply.github.com>
2026-05-09 17:47:26 +0100

    Update API_BASE_URL to new production endpoint

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'https://hls-new-api.onrender.com';
+export const API_BASE_URL = 'https://api.hlsnigeria.com';
 //export const API_BASE_URL = 'http://localhost:3000';
 export const CLOUDINARY_CLOUD_NAME = 'folajimidev';
 export const CLOUDINARY_UPLOAD_PRESET = 'zoahguuq';
