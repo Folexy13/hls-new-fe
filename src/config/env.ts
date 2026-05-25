44f129c91037c01109b8c5d56ba203844cef23b9
I'm Folajimi <62119915+Folexy13@users.noreply.github.com>
2026-05-25 08:59:13 +0100

    Update API_BASE_URL to remove default value
    Remove default value for API_BASE_URL.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.hlsnigeria.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
 export const CLOUDINARY_CLOUD_NAME = 'folajimidev';
 export const CLOUDINARY_UPLOAD_PRESET = 'zoahguuq';
